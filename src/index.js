c8499cd8600f86344b33d2e19ce03a468193ba8e
PrashantVashisth <prashantvashisth2004@gmail.com>
2025-02-06 18:25:47 +0530

    added a functionality of get user by id with some filtration.

--- src/index.js
+++ src/index.js
@@ -3,17 +3,22 @@ const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 const bodyParser = require('body-parser');
 const app = express();
+// const UserRepository = require('./repository/user-repository');
 
-const prepareAndStartServer = () => {
+const prepareAndStartServer = async () => {
 
     app.use(bodyParser.urlencoded({extended:true}));
     app.use(bodyParser.json());
     app.use('/api', apiRoutes); 
     
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         // console.log(PORT);
         console.log(`Server Started on Port: ${PORT}`);
-    })
+        // const userRepository = new UserRepository();
+        // const user = await userRepository.getById(1);
+        // console.log(`${user.email}   ${user.id}`);
+    });
+    
 }
 
 prepareAndStartServer();
